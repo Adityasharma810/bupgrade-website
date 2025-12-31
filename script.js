0b22db9b9caed1e561ba1c76989edf4cf1e6d17e
Aditya Sharma <ads626592@gmail.com>
2026-01-01 02:39:02 +0530

    Implement menu toggle functionality
    Added functionality to toggle navigation menu.

--- script.js
+++ script.js
@@ -7,3 +7,10 @@
       item.classList.toggle("open");
     });
   });
+
+const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav');
+
+toggle.addEventListener('click', () => {
+  nav.classList.toggle('open');
+});
